339c7c5326fc85dbfc7566639106559f45dc1efc
rgrgzz <ict_701@naver.com>
2025-11-29 16:25:23 +0900

    chore: 프로젝트 초기 구성

---
+++ src/main/java/com/example/wsdpractice/WsdpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.wsdpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsdpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsdpracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wsdpractice/WsdpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wsdpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsdpracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
